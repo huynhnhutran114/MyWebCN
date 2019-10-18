7b726870034991c26ad6a778db70e0f53231cdd6
Huynh Nhu <huynhnhutran114@gmail.com>
2019-10-18 14:40:45 +0700

    WebCN

---
+++ CN Scanner/Areas/Technician/TechnicianAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CN_Scanner.Areas.Technician
+{
+    public class TechnicianAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Technician";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Technician_default",
+                "Technician/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CN Scanner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CN_Scanner.MvcApplication" Language="C#" %>
